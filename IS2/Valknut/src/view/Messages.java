b93cf9a674b073f3c2b18fa18c6ea34e38509538
PCabello5 <pcabello.canales@gmail.com>
2026-02-24 15:31:19 +0100

    messages class for the ui

---
+++ IS2/Valknut/src/view/Messages.java
@@ -0,0 +1,10 @@
+package view;
+
+public class Messages {
+	
+	// Battle Miscellaneous
+	public static final String BATTLE_START = "NEW ENEMIES CROSS YOUR PATH. A NEW BATTLE STARTS";
+	public static final String BATTLE_WIN = "THE HEROES WIN. ONE STEP CLOSER TO VALHALLA.";
+	public static final String BATTLE_LOSS = "THE HEROES LOSE. YOUR ADVENTURE ENDS HERE.";
+	public static final String BATTLE_ESCAPE = "THE HEROES ESCAPED THE FIGTH.";
+}
