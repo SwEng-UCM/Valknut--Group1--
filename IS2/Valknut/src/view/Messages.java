b72e34662315422d357b06c9c8b74ef2d798df68
PCabello5 <pcabello.canales@gmail.com>
2026-02-26 12:06:18 +0100

    #23 Added messages for the basic enemies' attacks

--- IS2/Valknut/src/view/Messages.java
+++ IS2/Valknut/src/view/Messages.java
@@ -7,4 +7,11 @@ public class Messages {
 	public static final String BATTLE_WIN = "THE HEROES WIN. ONE STEP CLOSER TO VALHALLA.";
 	public static final String BATTLE_LOSS = "THE HEROES LOSE. YOUR ADVENTURE ENDS HERE.";
 	public static final String BATTLE_ESCAPE = "THE HEROES ESCAPED THE FIGTH.";
+	
+	// Enemies attacks
+	public static final String ICEGIANT_ATTACK = "The giant swings his club in your direction";
+	public static final String FIREGIANT_ATTACK = "The flaming monster charges at you with its sword";
+	public static final String SHAPESHIFTER_ATTACK = "In his animal form the shapeshifter slashes you with its claws";
+	public static final String BERSERKER_ATTACK = "The warrior launches a barrage of punches and axe slashes at you";
+	public static final String ELF_ATTACK = "The elf chanels the magic of the runes to harm you";
 }
