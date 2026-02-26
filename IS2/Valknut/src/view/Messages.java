151d1b7dad32a0630e772365dfc2349dc08a825f
PCabello5 <pcabello.canales@gmail.com>
2026-02-26 12:26:16 +0100

    #23 Some more messages

--- IS2/Valknut/src/view/Messages.java
+++ IS2/Valknut/src/view/Messages.java
@@ -7,6 +7,7 @@ public class Messages {
 	public static final String BATTLE_WIN = "THE HEROES WIN. ONE STEP CLOSER TO VALHALLA.";
 	public static final String BATTLE_LOSS = "THE HEROES LOSE. YOUR ADVENTURE ENDS HERE.";
 	public static final String BATTLE_ESCAPE = "THE HEROES ESCAPED THE FIGTH.";
+	public static final String ENEMY_TURN = "ENEMIES's TURN...";
 	
 	// Enemies attacks
 	public static final String ICEGIANT_ATTACK = "The giant swings his club in your direction";
@@ -14,4 +15,9 @@ public class Messages {
 	public static final String SHAPESHIFTER_ATTACK = "In his animal form the shapeshifter slashes you with its claws";
 	public static final String BERSERKER_ATTACK = "The warrior launches a barrage of punches and axe slashes at you";
 	public static final String ELF_ATTACK = "The elf chanels the magic of the runes to harm you";
+	public static final String ENEMY_MISS = "MISS: NO HERO FIGHTING"; // To be made into 2 different messages maybe
+	
+	// Player Actions
+	public static final String PLAYER_RUNS = "YOU ESCAPED TO FIGHT AGAIN HERO";
+	public static final String PLAYER_RUNFAIL = "YOU FAILED IN YOUR ESCAPE";
 }
