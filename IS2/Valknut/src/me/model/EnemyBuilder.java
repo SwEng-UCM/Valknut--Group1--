96faaa83dd92e9fa5863effdd96ae311378fefc5
Helio <238000184+helvega@users.noreply.github.com>
2026-03-16 22:23:20 +0100

    small fix

--- IS2/Valknut/src/me/model/EnemyBuilder.java
+++ IS2/Valknut/src/me/model/EnemyBuilder.java
@@ -14,21 +14,24 @@ public class EnemyBuilder {
     static public Enemy buildEnemy(String str){
         List<Integer> element = new ArrayList<>();
         switch (str.toLowerCase()) {
-            case "ice":
+            case "ice" -> {
                 Enemy iceGiant = new Enemy(Messages.ICEGIANT, 150, 200, 60, Messages.ICEGIANT_ATTACK);
                 element.add(2); element.add(1); element.add(1); element.add(1); element.add(1);
                 iceGiant.setElementStats(element);
                 return iceGiant;
-            case "fire":
+            }
+            case "fire" -> {
                 Enemy fireGiant = new Enemy(Messages.FIREGIANT, 150, 200, 70, Messages.FIREGIANT_ATTACK);
                 element.add(1); element.add(1); element.add(1); element.add(1); element.add(2);
                 fireGiant.setElementStats(element);
                 return fireGiant;
-            default:
+            }
+            default -> {
                 Enemy df = new Enemy(Messages.ICEGIANT, 150, 200, 60, Messages.ICEGIANT_ATTACK);
                 element.add(2); element.add(1); element.add(1); element.add(1); element.add(1);
                 df.setElementStats(element);
                 return df;
+            }
         }
     }
 }
