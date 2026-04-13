9ff5c8acf8ff5619c5facee9fc976a657a099c45
Helio <238000184+helvega@users.noreply.github.com>
2026-04-13 23:34:37 +0200

    x

--- IS2/Valknut/src/me/view/MainMenu.java
+++ IS2/Valknut/src/me/view/MainMenu.java
@@ -54,10 +54,10 @@ public class MainMenu extends JPanel{
         gbcMenu.fill = GridBagConstraints.NONE; 
         gbcMenu.anchor = GridBagConstraints.NORTH;
 
-        title = new JLabel(rescalate(1100, 600, new ImageIcon("resources/images/valknut_logo.png")));
+        title = new JLabel(rescalate(1200, 600, new ImageIcon("resources/images/valknut_logo.png")));
         gbcMenu.gridy = 0;
         gbcMenu.weighty = 0.0; 
-        gbcMenu.insets = new Insets(0, 0, -60, 0);
+        gbcMenu.insets = new Insets(-20, 0, -60, 0);
         this.add(title, gbcMenu);
 
         JPanel buttonPanel = new JPanel(new GridBagLayout());
@@ -77,14 +77,14 @@ public class MainMenu extends JPanel{
         btnMP = createButton("resources/images/multiButton_NS.png", "resources/images/multiButton_S.png");
         btnSettings = createButton("resources/images/settingsButton_NS.png", "resources/images/settingsButton_S.png");
 
-        gbc.gridx = 0;
+        gbc.gridy = 0;
         gbc.fill = GridBagConstraints.NONE;
-        gbc.insets = new Insets(-20, 0, 0, 0);
-        gbc.weighty = 0.0; 
+        gbc.insets = new Insets(250, 20, 0, 20);
+        gbc.weightx = 0.1; 
 
-        gbc.gridy = 0; buttonPanel.add(btnPlay, gbc);
-        gbc.gridy = 1; buttonPanel.add(btnMP, gbc);
-        gbc.gridy = 2; buttonPanel.add(btnSettings, gbc);
+        gbc.gridx = 1; buttonPanel.add(btnPlay, gbc);
+        gbc.gridx = 0; buttonPanel.add(btnMP, gbc);
+        gbc.gridx = 2; buttonPanel.add(btnSettings, gbc);
 
         this.add(buttonPanel, gbcMenu);
 
@@ -92,13 +92,13 @@ public class MainMenu extends JPanel{
 
     private JButton createButton(String path, String over){
         JButton jb = new JButton();
-        jb.setIcon(rescalate(400, 150, new ImageIcon(path)));
+        jb.setIcon(rescalate(300, 100, new ImageIcon(path)));
         jb.setContentAreaFilled(false); 
         jb.setBorderPainted(false);     
         jb.setFocusPainted(false);      
         jb.setOpaque(false);
         jb.setRolloverEnabled(true);
-        jb.setRolloverIcon(rescalate(400, 150, new ImageIcon(over)));
+        jb.setRolloverIcon(rescalate(300, 100, new ImageIcon(over)));
         jb.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
