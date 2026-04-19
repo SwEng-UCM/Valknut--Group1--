830bfcd0f5d956f9820023b24ce1b05d1af4210c
PCabello5 <pcabello.canales@gmail.com>
2026-04-19 18:47:04 +0200

    #67 added builder for all basic enemies which gives them their sprite

--- IS2/Valknut/src/me/model/EnemyBuilder.java
+++ IS2/Valknut/src/me/model/EnemyBuilder.java
@@ -28,6 +28,34 @@ public class EnemyBuilder {
                 fireGiant.setSprite("resources/images/Creatures/fire_giant.png");
                 return fireGiant;
             }
+            case "berserk" -> {
+            	Enemy berserk = new Enemy(Messages.BERSERKER, 100, 150, 55, Messages.BERSERKER_ATTACK);
+                element.add(1); element.add(1); element.add(1); element.add(2); element.add(1);
+                berserk.setElementStats(element);
+                berserk.setSprite("resources/images/Creatures/berserker.png");
+                return berserk;
+            }
+            case "shifter" -> {
+            	Enemy shifter = new Enemy(Messages.SHAPESHIFTER, 200, 250, 65, Messages.SHAPESHIFTER_ATTACK);
+                element.add(1); element.add(2); element.add(1); element.add(1); element.add(1);
+                shifter.setElementStats(element);
+                shifter.setSprite("resources/images/Creatures/bear_animal_shifter.png");
+                return shifter;
+            }
+            case "elf" -> {
+            	Enemy elf = new Enemy(Messages.ELF, 125, 150, 55, Messages.ELF_ATTACK);
+                element.add(1); element.add(1); element.add(2); element.add(1); element.add(1);
+                elf.setElementStats(element);
+                elf.setSprite("resources/images/Creatures/elf.png");
+                return elf;
+            }
+            case "draug" -> {
+            	Enemy draugr = new Enemy(Messages.DRAUGR, 100, 120, 60, Messages.DRAUGR_ATTACK);
+                element.add(1); element.add(3); element.add(1); element.add(1); element.add(1);
+                draugr.setElementStats(element);
+                draugr.setSprite("resources/images/Creatures/dragur.png");
+                return draugr;
+            }
             default -> {
                 Enemy df = new Enemy(Messages.ICEGIANT, 150, 200, 60, Messages.ICEGIANT_ATTACK);
                 element.add(2); element.add(1); element.add(1); element.add(1); element.add(1);
