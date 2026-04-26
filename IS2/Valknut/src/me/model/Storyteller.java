f243d599ab8e8ea9e4c063074cbe3e3846c9f65d
gadeadom <gadeadom@ucm.es>
2026-04-26 12:14:55 +0200

    Story teller 1&2 part def

--- IS2/Valknut/src/me/model/Storyteller.java
+++ IS2/Valknut/src/me/model/Storyteller.java
@@ -2,4 +2,18 @@ package me.model;
 
 public class Storyteller {
 
+	//*Players select characters
+
+	//Story starts Story.getIntro() Story.startFisrtChapter()
+
+	//First combat call combat with the first gigants
+
+	//Continiue story Story.middleFirstChapter()
+
+	//No matter what the player chooses Story.startSecondChapter(); Story.middleSecondChapter();
+
+	//Second combat call combat against the Dragurs
+
+	//....And so on
+	
 }
