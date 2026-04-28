a1e7afb122aca213328ea15e979bb328b440ce79
gadeadom <gadeadom@ucm.es>
2026-04-28 14:53:57 +0200

    StoryScreen next button

--- IS2/Valknut/src/me/view/StoryScreen.java
+++ IS2/Valknut/src/me/view/StoryScreen.java
@@ -1,5 +1,36 @@
 package me.view;
 
-public class StoryScreen {
+import java.awt.BorderLayout;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import me.control.Controller;
+
+public class StoryScreen extends JPanel {
+	private JButton nextButton;
+	private String text;
+	private Controller ctrl;
+	
+	StoryScreen(String text, Controller ctrl) {
+		this.text = text;
+		this.ctrl = ctrl;
+		initGui();
+	}
+
+	private void initGui() {
+		 setLayout(new BorderLayout());
+		 
+		 this.nextButton = new JButton();
+		 this.nextButton.setToolTipText("Load");
+		 this.nextButton.setIcon(new ImageIcon("resources/icons/open.png"));
+		 this.nextButton.addActionListener((e) -> handleNext());
+		 this.add(nextButton, BorderLayout.PAGE_END);
+		
+	}
+
+	private void handleNext() {
+		ctrl.next();
+	}
 }
